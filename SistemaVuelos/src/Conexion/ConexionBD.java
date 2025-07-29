7d2ff4c89343ff61b40ec92e9410d1766bbe648f
Eme2004 <emmairenase@est.utn.ac.cr>
2025-07-29 14:27:37 -0600

    Clase ConexionBD
    Realice la conexión con la base de datos y realice una prueba para poder identificar si esta realizaba la conexión

---
+++ SistemaVuelos/src/Conexion/ConexionBD.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author USER
+ */
+public class ConexionBD {
+    private static final String URL = "jdbc:postgresql://localhost:5432/sistema_reservas_vuelo";
+    private static final String USUARIO = "postgres";  
+    private static final String CONTRASENA = "eme12";  
+
+    public static Connection conectar() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+            System.out.println("✅ Conexión exitosa a PostgreSQL");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("❌ Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
+
+}
---
+++ SistemaVuelos/src/test/PruebaConexion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+import Conexion.ConexionBD;
+import java.sql.Connection;
+/**
+ *
+ * @author USER
+ */
+public class PruebaConexion {
+    public static void main(String[] args) {
+        Connection conn = ConexionBD.conectar();
+        if (conn != null) {
+            System.out.println("Conectado correctamente a la base de datos.");
+        } else {
+            System.out.println("No se pudo conectar.");
+        }
+    }
+}
