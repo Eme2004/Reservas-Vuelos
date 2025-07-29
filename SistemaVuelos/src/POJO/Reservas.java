a681b9088216dd8f3e141afa29826ea447389508
Eme2004 <emmairenase@est.utn.ac.cr>
2025-07-29 14:33:35 -0600

    Clases POJO
    Hice las clases de usuario, vuelo y reservas, con las variables y constructores pertinentes

--- SistemaVuelos/src/POJO/Reservas.java
+++ SistemaVuelos/src/POJO/Reservas.java
@@ -3,11 +3,72 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package POJO;
-
+import java.time.LocalDateTime;
 /**
  *
  * @author USER
  */
 public class Reservas {
+    private int idReserva;
+    private int idUsuario;
+    private int idVuelo;
+    private LocalDateTime fechaReserva;
+    private int cantidadAsientos;
+    
+    public Reservas(){}
+
+    public Reservas(int idReserva, int idUsuario, int idVuelo, LocalDateTime fechaReserva, int cantidadAsientos) {
+        this.idReserva = idReserva;
+        this.idUsuario = idUsuario;
+        this.idVuelo = idVuelo;
+        this.fechaReserva = fechaReserva;
+        this.cantidadAsientos = cantidadAsientos;
+    }
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdVuelo() {
+        return idVuelo;
+    }
+
+    public void setIdVuelo(int idVuelo) {
+        this.idVuelo = idVuelo;
+    }
+
+    public LocalDateTime getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(LocalDateTime fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public int getCantidadAsientos() {
+        return cantidadAsientos;
+    }
+
+    public void setCantidadAsientos(int cantidadAsientos) {
+        this.cantidadAsientos = cantidadAsientos;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservas{" + "idReserva=" + idReserva + ", idUsuario=" + idUsuario + ", idVuelo=" + idVuelo + ", fechaReserva=" + fechaReserva + ", cantidadAsientos=" + cantidadAsientos + '}';
+    }
+    
     
 }
--- SistemaVuelos/src/POJO/Vuelo.java
+++ SistemaVuelos/src/POJO/Vuelo.java
@@ -3,11 +3,114 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package POJO;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
 /**
  *
  * @author USER
  */
 public class Vuelo {
+    private int idVuelo;
+    private String aerolinea;
+    private String origen;
+    private String destino;
+    private LocalDate fechaSalida;
+    private LocalTime horaSalida;
+    private int escalas;
+    private double precio;
+    private int asientosDisponibles;
+    
+    
+    public Vuelo(){}
+
+    public Vuelo(int idVuelo, String aerolinea, String origen, String destino, LocalDate fechaSalida, LocalTime horaSalida, int escalas, double precio, int asientosDisponibles) {
+        this.idVuelo = idVuelo;
+        this.aerolinea = aerolinea;
+        this.origen = origen;
+        this.destino = destino;
+        this.fechaSalida = fechaSalida;
+        this.horaSalida = horaSalida;
+        this.escalas = escalas;
+        this.precio = precio;
+        this.asientosDisponibles = asientosDisponibles;
+    }
+
+    public int getIdVuelo() {
+        return idVuelo;
+    }
+
+    public void setIdVuelo(int idVuelo) {
+        this.idVuelo = idVuelo;
+    }
+
+    public String getAerolinea() {
+        return aerolinea;
+    }
+
+    public void setAerolinea(String aerolinea) {
+        this.aerolinea = aerolinea;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(LocalDate fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public LocalTime getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(LocalTime horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public int getEscalas() {
+        return escalas;
+    }
+
+    public void setEscalas(int escalas) {
+        this.escalas = escalas;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getAsientosDisponibles() {
+        return asientosDisponibles;
+    }
+
+    public void setAsientosDisponibles(int asientosDisponibles) {
+        this.asientosDisponibles = asientosDisponibles;
+    }
+
+    @Override
+    public String toString() {
+        return "Vuelo{" + "idVuelo=" + idVuelo + ", aerolinea=" + aerolinea + ", origen=" + origen + ", destino=" + destino + ", fechaSalida=" + fechaSalida + ", horaSalida=" + horaSalida + ", escalas=" + escalas + ", precio=" + precio + ", asientosDisponibles=" + asientosDisponibles + '}';
+    }
+    
     
 }
